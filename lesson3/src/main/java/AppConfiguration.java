617f2d3a2e2a188711d69efdbd48d2dfe4699da3
mpasichniuk <marypasichnyuk@gmail.com>
2022-09-05 19:18:17 +0200

    Spring3

---
+++ lesson3/src/main/java/AppConfiguration.java
@@ -0,0 +1,53 @@
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@EnableWebMvc
+@ComponentScan("java")
+@Configuration
+public class AppConfiguration implements WebMvcConfigurer {
+
+        private ApplicationContext context;
+
+        @Autowired
+        public void setApplicationContext(ApplicationContext context) {
+        this.context = context;
+        }
+
+@Override
+public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+        }
+
+@Bean
+public ViewResolver htmlViewResolver() {
+        ThymeleafViewResolver resolver = new ThymeleafViewResolver();
+        resolver.setTemplateEngine(templateEngine(htmlTemplateResolver()));
+        resolver.setContentType("text/html");
+        resolver.setCharacterEncoding("UTF-8");
+        resolver.setViewNames(new String[] {"*"});
+        return resolver;
+        }
+
+private ITemplateResolver htmlTemplateResolver() {
+        SpringResourceTemplateResolver resolver = new SpringResourceTemplateResolver();
+        resolver.setApplicationContext(applicationContext);
+        resolver.setPrefix("/WEB-INF/views/");
+        resolver.setSuffix(".html");
+        resolver.setCacheable(false);
+        resolver.setCharacterEncoding("UTF-8");
+        resolver.setTemplateMode(TemplateMode.HTML);
+        return resolver;
+        }
+
+private ISpringTemplateEngine templateEngine(ITemplateResolver templateResolver) {
+        SpringTemplateEngine engine = new SpringTemplateEngine();
+        engine.setTemplateResolver(templateResolver);
+        return engine;
+        }
+        }
+
+        }
